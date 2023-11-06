aaf2c75556d25270f0591c0bf0275cba7bc61dd5
abignego <iqbalmaarif232@gmail.com>
2023-11-06 09:20:47 +0700

    pembaharuan 2

--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -1,21 +1,27 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { navLinks } from "../data/index";
+import { NavLink } from "react-router-dom";
 
 const NavbarComponent = () => {
   return (
     <div>
-      <Navbar expand="lg" className="Navbar">
+      <Navbar className="navbar">
         <Container>
           <Navbar.Brand href="#home">Project XS-1</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="mx-auto">
               {navLinks.map((link) => {
                 return (
-                  <div key={link.id}>
-                    <Nav.Link href="">{link.text}</Nav.Link>
+                  <div className="nav-link" key={link.id}>
+                    <NavLink
+                      to={link.path}
+                      className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                      }
+                    >
+                      {link.text}
+                    </NavLink>
                   </div>
                 );
               })}
