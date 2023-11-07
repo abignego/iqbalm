021f1d347d1e0a115f9b1c775f141313fd1c404b
abignego <iqbalmaarif232@gmail.com>
2023-11-07 21:47:08 +0700

    uptodate

--- src/Pages/InsertWargaPages.jsx
+++ src/Pages/InsertWargaPages.jsx
@@ -2,12 +2,49 @@
 
 const InsertWargaPages = () => {
   return (
-    <div className="FormInputData">
-      <div className="input">
-        <label htmlFor="">Nama Lengkap</label>
-        <input className="input" type="email" name="" id="" placeholder="Masukan Nama" />
+    <form action="#">
+
+  
+        <div className="FormInputData" >
+            <h4>Masukan Data Diri !!!</h4> 
+      <div className="ContainerIn">
+      <div className="input-box">
+        <span >Nama Lengkap</span>
+        <input onInvalid={"Masukan nma"} className="Inputed" type="text" name="" id="" placeholder="Masukan Nama" />
       </div>
+      <div className="input-box">
+        <span >NIK</span>
+        <input onInvalid={"Masukan nma"} className="Inputed" type="text" name="" id="" placeholder="Masukan Nik" />
+      </div>
+      <div className="input-box">
+        <span >Tempat</span>
+        <input onInvalid={"Masukan nma"} className="Inputed" type="text" name="" id="" placeholder="Masukan Nik" />
+      </div>
+      <div className="input-box">
+        <span >Tanggal Lahir</span>
+        <input onInvalid={"Masukan nma"} className="Inputed" type="date" name="" id="" placeholder="Masukan Nik" />
+      </div>
+      <div className="input-radio">
+        <span >Ranting</span>
+        <select name="ranting" id="ranting">
+    <option value="Ranting Gondrong">Ranting Gondrong</option>
+    <option value="Ranting Petir">Ranting Petir</option>
+    <option value="Ranting Cipondoh">Ranting Cipondoh</option>
+    <option value="Ranting Ketapang">Ranting Ketapang</option>
+    <option value="Ranting Kenanga">Ranting Kenanga</option>
+    <option value="Ranting Poris">Ranting Poris</option>
+  </select>
+       
+      </div>
+
+      <div className="button">
+
+      <input type="submit" className="btn btn-primary" value="Register"/>
+      </div>
+        </div> 
     </div>
+    </form>
+      
   )
 }
 
